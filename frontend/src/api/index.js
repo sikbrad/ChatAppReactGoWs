48ab3ca59968ea17a7f2fe9544786921dc882c2d
sikbrad <sikbrad@gmail.com>
2020-12-27 22:50:41 +0900

    tut2 fin

---
+++ frontend/src/api/index.js
@@ -0,0 +1,30 @@
+var socket = new WebSocket("ws://localhost:8080/ws");
+
+let connect = () => {
+    console.log("Attempting connection");
+
+    socket.onopen = () => {
+        console.log("Successfully connected")
+    };
+
+    socket.onmessage = (msg) => {
+        console.log(msg)
+    };
+
+    socket.onclose = (event) => {
+        console.log("Socket closed connection : ",event)
+    };
+
+    socket.onerror = (error) => {
+        console.log("Socket error : ", error)
+    };
+};
+
+let sendMsg = (msg) => {
+    console.log("sending msg : ",msg)
+    socket.send(msg)
+}
+
+export {
+    connect, sendMsg
+};
