5746e8698f941f700ada2d16683c2adfd437c2bb
sikbrad <sikbrad@gmail.com>
2020-12-27 22:37:13 +0900

    upto ws setup

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,51 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+//define websocket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Host : %v\n", r.Host)
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	//listen indefinitely
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		//read
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		//print
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Chat app v0.0.1 started")
@@ -17,6 +60,8 @@ func main() {
 func setupRoutes() {
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf("Simple server is open")
+			fmt.Fprintf(w, "Simple server is open")
 		})
+
+	http.HandleFunc("/ws", serveWs)
 }
